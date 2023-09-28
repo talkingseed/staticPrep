af7f8f5e04624c58580b1e9c8095baa05c1e9c13
talkingseed <talkingseed@gmail.com>
2023-09-28 10:29:36 -0700

    Update dist.sh

--- dist.sh
+++ dist.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+
+# Scripts for image prep
+#./export_img_srcset.sh -d employer_logo/ -h 24
+#echo Done exporting employer logos
+#./export_img_thumbs.sh -d memes/ -h 136
+#echo Done exporting memes and their thumbs
+
+./css_insert_fallback.pl CSS/home.css > CSS/home_dist.css
+./css_insert_fallback.pl CSS/quote.css > CSS/quote_dist.css
+echo Inserted fallbacks to css
 npx postcss CSS/reset.css > CSS/reset_min.css
-npx postcss CSS/home.css > CSS/home_min.css
+npx postcss CSS/home_dist.css > CSS/home_dist_min.css
+rm CSS/home_dist.css
+npx postcss CSS/quote_dist.css > CSS/quote_dist_min.css
+rm CSS/quote_dist.css
+echo minified css
 npx swc --quiet --config-file ../.swcrc  JS/comfySlider.js > JS/comfySlider_min.js
-php index.php > index.html
-echo Done!
+npx swc --quiet --config-file ../.swcrc  JS/togglePrefTheme.js > JS/togglePrefTheme_min.js
+echo minified js
